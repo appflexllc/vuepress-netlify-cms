512d38f6a9969cfc407abfc2a557b4e2e48dc673
appflexllc <appflexllc@gmail.com>
2018-11-13 00:26:34 -0500

    2nd

---
+++ .vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'Outreach',
+	description: 'A place for my stuff',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Blog', link: '/blog/' }
+        ],
+        sidebar: [
+            '/',
+            '/blog/'
+        ]
+    }
+}
